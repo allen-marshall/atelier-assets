5a2238a26f1c0dc1b3cae18d827b836f06c932c3
Allen Marshall <allen.marshall.22@gmail.com>
2020-02-24 20:54:35 -0600

    Add more AsRef wrappers to kv-store
    This is a workaround for a Rust compiler bug/limitation: https://github.com/rust-lang/rust/issues/56556

--- kv-store/src/lt_trait_wrappers.rs
+++ kv-store/src/lt_trait_wrappers.rs
@@ -4,6 +4,38 @@
 //!
 //! [issue]: https://github.com/rust-lang/rust/issues/56556
 
+/// Wrapper that adds 2 unused lifetime parameters to [`AsRef`][AsRef].
+///
+/// [AsRef]: std::convert::AsRef
+pub trait AsRefLt2<'lt0, 'lt1, T>: AsRef<T>
+where
+    T: ?Sized,
+{
+}
+
+impl<'lt0, 'lt1, T, I> AsRefLt2<'lt0, 'lt1, T> for I
+where
+    I: ?Sized + AsRef<T>,
+    T: ?Sized,
+{
+}
+
+/// Wrapper that adds 4 unused lifetime parameters to [`AsRef`][AsRef].
+///
+/// [AsRef]: std::convert::AsRef
+pub trait AsRefLt4<'lt0, 'lt1, 'lt2, 'lt3, T>: AsRef<T>
+where
+    T: ?Sized,
+{
+}
+
+impl<'lt0, 'lt1, 'lt2, 'lt3, T, I> AsRefLt4<'lt0, 'lt1, 'lt2, 'lt3, T> for I
+where
+    I: ?Sized + AsRef<T>,
+    T: ?Sized,
+{
+}
+
 /// Wrapper that adds 6 unused lifetime parameters to [`AsRef`][AsRef].
 ///
 /// [AsRef]: std::convert::AsRef
